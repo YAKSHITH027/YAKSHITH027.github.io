ece156638390693511ebda5b0b56557da48e78da
yakshith <yakshith027@gmail.com>
2023-04-04 14:08:27 +0530

    check once

--- src/utils/Data/Projects.js
+++ src/utils/Data/Projects.js
@@ -1,47 +1,47 @@
-import image1 from "../Images/image1.png";
-import image2 from "../Images/image2.png";
-import image3 from "../Images/image3.png";
-import image4 from "../Images/image4.png";
+import image1 from '../Images/image1.png'
+import image2 from '../Images/image2.png'
+import image3 from '../Images/image3.png'
+import image4 from '../Images/image4.png'
 
 export const projectsList = [
   {
     id: 4,
-    name: "Attirely",
-    liveLink: "https://attirely.netlify.app/",
-    githubLink: "https://github.com/YAKSHITH027/permissible-cloth-4922",
+    name: 'Attirely ',
+    liveLink: 'https://attirely.netlify.app/',
+    githubLink: 'https://github.com/YAKSHITH027/permissible-cloth-4922',
     image: image4,
-    techStacks: ["react", "redux", "chakra ui", "firebase"],
+    techStacks: ['react', 'redux', 'chakra ui', 'firebase'],
     desc: `it's a clone of Myntra, a website 
 for men's and women's clothing`,
   },
   {
     id: 1,
-    name: "weartrend",
-    liveLink: "https://melodic-tartufo-e97294.netlify.app/",
-    githubLink: "https://github.com/YAKSHITH027/eminent-tin-5074",
+    name: 'weartrend',
+    liveLink: 'https://melodic-tartufo-e97294.netlify.app/',
+    githubLink: 'https://github.com/YAKSHITH027/eminent-tin-5074',
     image: image1,
-    techStacks: ["react", "chakra ui", "firebase", "json-servor"],
+    techStacks: ['react', 'chakra ui', 'firebase', 'json-servor'],
     desc: `it's a clone of bloomingdales, an online shopping site
 for men's and women's clothing`,
   },
   {
     id: 12,
-    name: "Shopify",
-    liveLink: "https://chipper-creponne-aa2b3e.netlify.app/",
-    githubLink: "https://github.com/YAKSHITH027/illustrious-feeling-7979",
+    name: 'Shopify',
+    liveLink: 'https://chipper-creponne-aa2b3e.netlify.app/',
+    githubLink: 'https://github.com/YAKSHITH027/illustrious-feeling-7979',
     image: image3,
-    techStacks: ["Javascript", "CSS", "HTML", "json-servor"],
+    techStacks: ['Javascript', 'CSS', 'HTML', 'json-servor'],
     desc: `It is an e-commerce website for buying home appliances and other accessories `,
   },
   {
     id: 13,
-    name: "Tripify",
-    liveLink: "https://rad-cannoli-e0c5be.netlify.app/",
-    githubLink: "https://github.com/YAKSHITH027/tart-seat-6454",
+    name: 'Tripify',
+    liveLink: 'https://rad-cannoli-e0c5be.netlify.app/',
+    githubLink: 'https://github.com/YAKSHITH027/tart-seat-6454',
     image: image2,
-    techStacks: ["Javascript", "CSS", "HTML"],
+    techStacks: ['Javascript', 'CSS', 'HTML'],
     desc: `Tripoto is a platform to share and discover
 amazing travel stories and itineraries.
 `,
   },
-];
+]
