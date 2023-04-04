63ade09cdbe75931ae846af7ab1437283e1cebd9
yakshith <yakshith027@gmail.com>
2023-04-04 15:19:58 +0530

    kept link inside

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,84 +1,81 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Link,
-  Text,
-} from "@chakra-ui/react";
-import React from "react";
-import { BsMouse } from "react-icons/bs";
-import { ImLinkedin } from "react-icons/im";
-import { FaGithub } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
-import { AiOutlineArrowDown, AiOutlineDownload } from "react-icons/ai";
-import resume from "../utils/Resume/P_Yakshith_Resume.pdf";
-import Fade from "react-reveal/Fade";
-import Zoom from "react-reveal/Zoom";
-import openInNewTab from "../utils/newTabOpener";
+import { Box, Button, Flex, Heading, Image, Link, Text } from '@chakra-ui/react'
+import React from 'react'
+import { BsMouse } from 'react-icons/bs'
+import { ImLinkedin } from 'react-icons/im'
+import { FaGithub } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { AiOutlineArrowDown, AiOutlineDownload } from 'react-icons/ai'
+import resume from '../utils/Resume/P_Yakshith_Resume.pdf'
+import Fade from 'react-reveal/Fade'
+import Zoom from 'react-reveal/Zoom'
+import openInNewTab from '../utils/newTabOpener'
 
 const Home = () => {
   return (
     <Flex
-      height={{ base: "80vh", lg: "92vh" }}
-      maxHeight={"750px"}
-      className="background"
-      id="home"
-      flexDir={{ base: "column-reverse", lg: "row" }}
-      align={"center"}
-      justify="center"
-      gap={{ base: "4", md: "6", lg: "8" }}
+      height={{ base: '80vh', lg: '92vh' }}
+      maxHeight={'750px'}
+      className='background'
+      id='home'
+      flexDir={{ base: 'column-reverse', lg: 'row' }}
+      align={'center'}
+      justify='center'
+      gap={{ base: '4', md: '6', lg: '8' }}
     >
       <Flex
-        width={{ base: "100%", lg: "40%" }}
-        flexDir="column"
-        gap="1rem"
-        padding={"5px"}
-        pl={{ base: "2rem", lg: "4rem" }}
+        width={{ base: '100%', lg: '40%' }}
+        flexDir='column'
+        gap='1rem'
+        padding={'5px'}
+        pl={{ base: '2rem', lg: '4rem' }}
       >
         <Fade left>
           <Heading>Hello There 👋</Heading>
-          <Heading id="user-detail-name">I am Yakshith</Heading>
-          <Text fontSize={"1.2rem"}>
+          <Heading id='user-detail-name'>I am Yakshith</Heading>
+          <Text fontSize={'1.2rem'}>
             A Passionate Full Stack Web Developer who's curious in tech field,
             and now looking to start a career as an Web Developer.
           </Text>
           <Text></Text>
-          <Flex gap={"8"} align="center">
-            <Link href={resume} download id="resume-link-2">
-              <Button
-                id="resume-button-2"
-                width="max-content"
-                onClick={() => openInNewTab(resume)}
-                colorScheme={"teal"}
-              >
-                Resume
-                <Text as="span" ml={"2"}>
-                  <AiOutlineDownload fontSize={"1.2rem"} />
-                </Text>
-              </Button>
-            </Link>
-            <Flex gap="3">
+          <Flex gap={'8'} align='center'>
+            <Button
+              id='resume-button-2'
+              onClick={() =>
+                openInNewTab(
+                  'https://drive.google.com/file/d/1iRtaj8LV9l5y9-aiEVNRFC0KLz5Q-JMZ/view?usp=sharing'
+                )
+              }
+              colorScheme={'teal'}
+            >
+              <Link href={resume} download id='resume-link-2'>
+                <Flex>
+                  <Text as='span'>Resume</Text>
+                  <Text as='span' ml={'2'}>
+                    <AiOutlineDownload fontSize={'1.2rem'} />
+                  </Text>
+                </Flex>
+              </Link>
+            </Button>
+            <Flex gap='3'>
               <Link
-                href="https://www.linkedin.com/in/p-yakshith-kulal-490b34227/"
+                href='https://www.linkedin.com/in/p-yakshith-kulal-490b34227/'
                 isExternal
               >
-                <ImLinkedin fontSize={"1.6rem"} />
+                <ImLinkedin fontSize={'1.6rem'} />
               </Link>
-              <Link href="https://github.com/YAKSHITH027" isExternal>
-                <FaGithub fontSize={"1.6rem"} />
+              <Link href='https://github.com/YAKSHITH027' isExternal>
+                <FaGithub fontSize={'1.6rem'} />
               </Link>
-              <Link href="mailto:yakshith027@gmail.com" isExternal>
-                <HiOutlineMail fontSize={"1.6rem"} />
+              <Link href='mailto:yakshith027@gmail.com' isExternal>
+                <HiOutlineMail fontSize={'1.6rem'} />
               </Link>
             </Flex>
           </Flex>
 
-          <Box marginTop={"4rem"} display={{ base: "none", lg: "block" }}>
-            <Flex align={"center"} gap="2">
+          <Box marginTop={'4rem'} display={{ base: 'none', lg: 'block' }}>
+            <Flex align={'center'} gap='2'>
               <span>
-                <BsMouse fontSize={"1.4rem"} />
+                <BsMouse fontSize={'1.4rem'} />
               </span>
               <Text>scroll down</Text>
               <span>
@@ -91,19 +88,19 @@ const Home = () => {
       <Zoom>
         <Box
           // height={{ base: "200px", md: "300px", lg: "400px" }}
-          overflow="hidden"
-          className="profile"
+          overflow='hidden'
+          className='profile'
         >
           <Image
-            src="https://camo.githubusercontent.com/86a3b6db470f1a0429f7355c08d1edabf3d2c804/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f313336302f312a495247486d69477361313673746564517649615a66772e676966"
-            className="imageShadow"
-            height={{ base: "196px", md: "280px", lg: "396px" }}
-            width={{ base: "312px", md: "490px", lg: "630px" }}
+            src='https://camo.githubusercontent.com/86a3b6db470f1a0429f7355c08d1edabf3d2c804/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f313336302f312a495247486d69477361313673746564517649615a66772e676966'
+            className='imageShadow'
+            height={{ base: '196px', md: '280px', lg: '396px' }}
+            width={{ base: '312px', md: '490px', lg: '630px' }}
           />
         </Box>
       </Zoom>
     </Flex>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,92 +6,103 @@ import {
   Link,
   Text,
   useColorMode,
-} from "@chakra-ui/react";
-import React from "react";
-import { MoonIcon, SunIcon, HamburgerIcon } from "@chakra-ui/icons";
-import Sidebar from "./Sidebar";
-import { AiOutlineDownload } from "react-icons/ai";
-import resume from "../utils/Resume/P_Yakshith_Resume.pdf";
-import Fade from "react-reveal/Fade";
-import openInNewTab from "../utils/newTabOpener";
+} from '@chakra-ui/react'
+import React from 'react'
+import { MoonIcon, SunIcon, HamburgerIcon } from '@chakra-ui/icons'
+import Sidebar from './Sidebar'
+import { AiOutlineDownload } from 'react-icons/ai'
+import resume from '../utils/Resume/P_Yakshith_Resume.pdf'
+import Fade from 'react-reveal/Fade'
+import openInNewTab from '../utils/newTabOpener'
 const Navbar = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode()
 
   return (
     <Box
-      id="nav-menu"
-      pos={"sticky"}
-      top="0"
-      textTransform={"capitalize"}
-      zIndex="1000"
+      id='nav-menu'
+      pos={'sticky'}
+      top='0'
+      textTransform={'capitalize'}
+      zIndex='1000'
     >
       <Fade top>
         <Flex
-          justify={"space-between"}
-          height="4rem"
-          align={"center"}
-          transition={"all 1s"}
+          justify={'space-between'}
+          height='4rem'
+          align={'center'}
+          transition={'all 1s'}
         >
-          <Box height="3.5rem" marginLeft={{ base: "2px", lg: "2rem" }}>
-            <Link href="#">
+          <Box height='3.5rem' marginLeft={{ base: '2px', lg: '2rem' }}>
+            <Link href='#'>
               <Image
                 src={
-                  colorMode == "dark"
-                    ? "https://i.ibb.co/Rj0j6jc/Yakshith-1-removebg-preview.png"
-                    : "https://i.ibb.co/vYtc4mH/Yakshith-removebg-preview.png"
+                  colorMode == 'dark'
+                    ? 'https://i.ibb.co/Rj0j6jc/Yakshith-1-removebg-preview.png'
+                    : 'https://i.ibb.co/vYtc4mH/Yakshith-removebg-preview.png'
                 }
-                height={"3.5rem"}
+                height={'3.5rem'}
               />
             </Link>
           </Box>
 
           <Flex
-            display={{ base: "none", lg: "flex" }}
-            width={"50%"}
-            justify="space-around"
-            align={"center"}
-            marginRight="2rem"
+            display={{ base: 'none', lg: 'flex' }}
+            width={'50%'}
+            justify='space-around'
+            align={'center'}
+            marginRight='2rem'
           >
-            <Link href="#home" className="nav-link home">
+            <Link href='#home' className='nav-link home'>
               home
             </Link>
-            <Link href="#about" className="nav-link about">
+            <Link href='#about' className='nav-link about'>
               about
             </Link>
-            <Link href="#skills" className="nav-link skills">
+            <Link href='#skills' className='nav-link skills'>
               skills
             </Link>
-            <Link href="#projects" className="nav-link projects">
+            <Link href='#projects' className='nav-link projects'>
               projects
             </Link>
-            <Link href="#contact" className="nav-link contact">
+            <Link href='#contact' className='nav-link contact'>
               contact
             </Link>
 
-            <Link href={resume} download className="nav-link resume">
-              <Button
-                id="resume-button-1"
-                width="max-content"
-                colorScheme={"teal"}
-                onClick={() => openInNewTab(resume)}
+            <Button
+              id='resume-button-1'
+              width='max-content'
+              colorScheme={'teal'}
+              onClick={() =>
+                openInNewTab(
+                  'https://drive.google.com/file/d/1iRtaj8LV9l5y9-aiEVNRFC0KLz5Q-JMZ/view?usp=sharing'
+                )
+              }
+            >
+              <Link
+                href={resume}
+                download
+                className='nav-link resume'
+                id='resume-link-1'
               >
-                Resume
-                <Text as="span" ml={"2"}>
-                  <AiOutlineDownload fontSize={"1.2rem"} />
-                </Text>
-              </Button>
-            </Link>
+                <Flex>
+                  <Text>Resume</Text>
+                  <Text as='span' ml={'2'}>
+                    <AiOutlineDownload fontSize={'1.2rem'} />
+                  </Text>
+                </Flex>
+              </Link>
+            </Button>
             <Button onClick={toggleColorMode}>
-              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
             </Button>
           </Flex>
-          <Box display={{ lg: "none" }} marginRight="2rem">
+          <Box display={{ lg: 'none' }} marginRight='2rem'>
             <Sidebar />
           </Box>
         </Flex>
       </Fade>
     </Box>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
